492a0ef8f836fbaac780449e489ec76db6238a66
apmArka <apmarka2003@gmail.com>
2022-08-27 16:27:56 +0530

    firstcommit

--- Blog-Final/filter.js
+++ Blog-Final/filter.js
@@ -35,15 +35,3 @@ function w3RemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-/*
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("btnContainer");
-var btns = btnContainer.getElementsByClassName("btnf");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("activef");
-    current[0].className = current[0].className.replace(" activef", "");
-    this.className += " activef";
-  });
-}
- */
